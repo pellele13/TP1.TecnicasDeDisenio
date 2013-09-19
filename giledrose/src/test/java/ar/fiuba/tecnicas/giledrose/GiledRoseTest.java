1485a159e196c31c09d45af231a994ca729d9b5c
pellele13 <lrpellegrino@gmail.com>
2013-09-18 23:45:47 -0300

    Se eligio el patro Strategy para poder updatear el inventario. Se armo el paquete refactor donde estaran las clases relacionadas con el patron.

--- giledrose/src/test/java/ar/fiuba/tecnicas/giledrose/GiledRoseTest.java
+++ giledrose/src/test/java/ar/fiuba/tecnicas/giledrose/GiledRoseTest.java
@@ -1,12 +1,18 @@
 package ar.fiuba.tecnicas.giledrose;
 
+import java.util.Collection;
+
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 
 public class GiledRoseTest {
-    @Test
+	
+	private Inventory instance;
+	private Collection<Item> arreglo;
+	
+	@Test
     public void replaceWithRealTests() {
         assertEquals("replace with real tests!", false, true);
     }
