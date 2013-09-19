76bcb96bbaae6ad1ae9ad63328694c58f35e7847
pellele13 <lrpellegrino@gmail.com>
2013-09-19 17:25:59 -0300

    clase que actualiza BackstagePasses

---
+++ giledrose/src/test/java/ar/fiuba/tecnicas/giledrose/UpdateStrategyBackstagePassesItem.java
@@ -0,0 +1,27 @@
+package ar.fiuba.tecnicas.giledrose;
+
+import ar.fiuba.tecnicas.giledroseStrategy.*;
+
+public class UpdateStrategyBackstagePassesItem implements UpdateStrategy {
+
+	@Override
+	public void update(Item item) {
+
+		if(item.getQuality() < 50){
+			if(item.getSellIn() <= 0){
+				item.setQuality(0);
+			}else if(item.getSellIn() < 6){
+				item.setQuality(item.getQuality()+3);
+			}else if(item.getSellIn() < 11){
+				item.setQuality(item.getQuality()+2);
+			}else{
+				item.setQuality(item.getQuality()+1);
+			}
+		}
+		if(item.getQuality() > 50){
+			item.setQuality(50);
+		}
+	}
+	
+	
+}
